ac4aeadc6a3c84c35d9e7bf2b2c830388815d423
dement0r <abdelhamidnajmi10@gmail.com>
2023-01-08 12:32:06 +0100

    added authentification

--- app/src/main/java/com/example/mobileproject/LoginActivity.java
+++ app/src/main/java/com/example/mobileproject/LoginActivity.java
@@ -1,29 +1,81 @@
 package com.example.mobileproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.jetbrains.annotations.NotNull;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email_login;
+    EditText password_login;
+    TextView signup_instead;
+    Button btnLogin;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-    }
-    public void onLoginBtnClicked(View view) {
-        Intent feed = toFeed();
-        startActivity(feed);
+
+        email_login = (EditText) findViewById(R.id.email_field_login);
+        password_login = (EditText) findViewById(R.id.password_field_login);
+        signup_instead = (TextView) findViewById(R.id.no_account);
+        btnLogin = (Button) findViewById(R.id.login_button);
+        mAuth = FirebaseAuth.getInstance();
+        btnLogin.setOnClickListener(view -> {
+            loginUser();
+        });
+        signup_instead.setOnClickListener(view -> {
+            startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            finish();
+        });
+
     }
 
-    Intent toFeed(){
-        return new Intent(this, MainfeedActivity.class);
+    private void loginUser(){
+        String email = email_login.getText().toString();
+        String password = password_login.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            email_login.setError("Email cannot be empty");
+            email_login.requestFocus();
+        }else if (TextUtils.isEmpty(password)){
+            password_login.setError("Password cannot be empty");
+            password_login.requestFocus();
+        }else{
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "User logged in successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, MainfeedActivity.class));
+                        finish();
+                    }else {
+                        Toast.makeText(LoginActivity.this, "Log in Error: "+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 
-    public void SignUp(View view) {
-        Intent Sign = new Intent(this, SignupActivity.class);
-        startActivity(Sign);
-    }
 }
--- app/src/main/java/com/example/mobileproject/SignupActivity.java
+++ app/src/main/java/com/example/mobileproject/SignupActivity.java
@@ -1,14 +1,83 @@
 package com.example.mobileproject;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
 public class SignupActivity extends AppCompatActivity {
 
+    EditText email_signup;
+    EditText password_signup;
+    EditText fullname_signup;
+    EditText username_signup;
+    EditText phonenumber_signup;
+    TextView login_instead;
+    Button btnRegister;
+
+    FirebaseAuth mAuth;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
+        fullname_signup = (EditText) findViewById(R.id.fullname_field_signup);
+        email_signup = (EditText) findViewById(R.id.email_field_signup);
+        password_signup = (EditText) findViewById(R.id.password_field_signup);
+        username_signup = (EditText) findViewById(R.id.username_field_signup);
+        phonenumber_signup = (EditText) findViewById(R.id.phonenumber_field_signup);
+        btnRegister = (Button) findViewById(R.id.sign_up_button);
+        login_instead = (TextView) findViewById(R.id.have_account);
+        mAuth = FirebaseAuth.getInstance();
+        btnRegister.setOnClickListener(view -> createUser());
+        login_instead.setOnClickListener(view -> {
+            startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+            finish();
+        });
+
+    }
+    private void createUser() {
+        String fullname = fullname_signup.getText().toString();
+        String username = username_signup.getText().toString();
+        String email = email_signup.getText().toString();
+        String phonenumber = phonenumber_signup.getText().toString();
+        String password = password_signup.getText().toString();
+
+        if (TextUtils.isEmpty(fullname)){
+            fullname_signup.setError("Full Name cannot be empty");
+            fullname_signup.requestFocus();
+        }else if (TextUtils.isEmpty(username)){
+            username_signup.setError("Username cannot be empty");
+            username_signup.requestFocus();
+        }else if (TextUtils.isEmpty(email)) {
+            email_signup.setError("Email cannot be empty");
+            email_signup.requestFocus();
+        }else if (TextUtils.isEmpty(phonenumber)){
+            phonenumber_signup.setError("Phone Number cannot be empty");
+            phonenumber_signup.requestFocus();
+        }else if (TextUtils.isEmpty(password)) {
+            password_signup.setError("Password cannot be empty");
+            password_signup.requestFocus();
+        }else{
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+                    Toast.makeText(SignupActivity.this, "User registered Successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(SignupActivity.this, MainfeedActivity.class));
+                }else{
+                    Toast.makeText(SignupActivity.this, "Registration Error: " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
